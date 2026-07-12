5b65a78a2bf0d25d3873e7b7ef00b3df4bea5e36
Andyloris <63070052+Andyloris@users.noreply.github.com>
2026-07-12 23:03:50 +0200

    Testing script

---
+++ test_patch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+BOOK="$1"
+
+mkdir -p test/
+RUSTFLAGS="-C target-cpu=native" cargo build --release
+cp target/release/final_sardine test/eng1
+
+git checkout HEAD^1
+RUSTFLAGS="-C target-cpu=native" cargo build --release
+cp target/release/final_sardine test/eng2
+
+git switch -
+fastchess -engine cmd=./test/eng1 name=NewSardine \
+	-engine cmd=./test/eng2 name=OldSardine \
+	-each tc=8+0.08 \
+	-rounds 1000000 \
+	-repeat \
+	-concurrency 15 \
+	-recover \
+	-openings file="$BOOK" format=pgn order=random \
+	-sprt elo0=0 elo1=10 alpha=0.05 beta=0.05 model=logistic
